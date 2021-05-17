918c885de38944a789313c7cc746f5103c31f5bf
PC <PC@DESKTOP-QORBUC8>
2021-05-17 07:18:00 +0200

    Agregar archivos de proyecto.

---
+++ ProyectoFinal_Instragram/Presentacion/Login/Login_Inicio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal_Instragram.Presentacion.Login
+{
+    public partial class Login_Inicio : Form
+    {
+        public Login_Inicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
